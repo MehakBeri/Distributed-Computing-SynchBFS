a347d3f1ae1290c20ca15c7aba16c088835ff22a
MehakBeri <mehakberi1996@gmail.com>
2019-02-02 14:59:29 -0600

    feb2_first commit

---
+++ proj1.py
@@ -0,0 +1,16 @@
+import threading
+import time
+import sys
+
+
+
+if __name__=="__main__":
+    assert(len(sys.argv) == 3)
+    n = sys.argv[1]
+    root = sys.argv[2]
+    ids = [1,2,3,4,5]
+    root = 1
+    #connectivity_matrix =  [[1 0 0 0 1], [0 1 1 0 0], [1 0 1 0 1], [0 1 0 1 0], [0 1 0 0 1]]
+
+
+
